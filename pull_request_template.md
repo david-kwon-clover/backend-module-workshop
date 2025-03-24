ef423f0264dca92be05e83e5ed73c920e0a7786c
David Kwon <david.kwon@clover.com>
2025-03-24 10:13:14 -0600

    Initial commit

---
+++ pull_request_template.md
@@ -0,0 +1,9 @@
+## Description
+Brief description of the changes made.
+
+## Type of Change
+- [ ] Bug fix
+- [ ] New feature
+- [ ] Breaking change
+- [ ] Documentation update
+
---
+++ src/main/java/com/multiverse/Snipper/SnipperApplication.java
@@ -0,0 +1,13 @@
+package com.multiverse.Snipper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnipperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnipperApplication.class, args);
+	}
+
+}
