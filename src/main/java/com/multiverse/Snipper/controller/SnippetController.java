54e3439489032299c7f02d176ef274ef200418ad
David Kwon <david.kwon@clover.com>
2025-03-25 10:42:49 -0600

    Clean up

--- src/main/java/com/multiverse/Snipper/controller/SnippetController.java
+++ src/main/java/com/multiverse/Snipper/controller/SnippetController.java
@@ -2,19 +2,10 @@ package com.multiverse.Snipper.controller;
 
 import com.multiverse.Snipper.model.Snippet;
 import com.multiverse.Snipper.service.SnippetServiceImpl;
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
